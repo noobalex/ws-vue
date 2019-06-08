942176a5e7ce8890f5bd851286b44eccb91ae795
bxd <xiaodong.me@gmail.com>
2019-06-08 22:17:40 +0800

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,56 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    {{message}}
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        data() {
+            return {
+                shopId: '',
+                message: ''
+            }
+        },
+        created() { // 页面创建生命周期函数
+              this.initWebSocket()
+        },
+        destroyed: function () { // 离开页面生命周期函数
+              this.websocketclose();
+        },
+        methods: {
+            // collapse: function(){
+            //     this.isCollapse = !this.isCollapse;
+            //     if (this.isCollapse) {
+            //         this.iconClass = "cebianlanzhankai";
+            //     } else{
+            //         this.iconClass = "cebianlanshouhui";
+            //     }
+            // },
+            initWebSocket: function () {
+                // WebSocket与普通的请求所用协议有所不同，ws等同于http，wss等同于https
+                this.websock = new WebSocket("ws://localhost:8080/websocket/DPS007");
+                this.websock.onopen = this.websocketonopen;
+                this.websock.onerror = this.websocketonerror;
+                this.websock.onmessage = this.websocketonmessage;
+                this.websock.onclose = this.websocketclose;
+              },
+              websocketonopen: function () {
+                console.log("WebSocket连接成功");
+              },
+              websocketonerror: function (e) {
+                console.log("WebSocket连接发生错误");
+              },
+              websocketonmessage: function (e) {
+                var da = e.data;
+                console.log(da);
+                this.message = da;
+              },
+              websocketclose: function (e) {
+                console.log("connection closed (" + e.code + ")");
+              }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
